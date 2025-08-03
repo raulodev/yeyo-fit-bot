1ed5dbb366d06b599d8d3c9c52e44e4f250904f3
raulodev <raulodev@gmail.com>
2025-08-03 15:23:22 -0400

    remove webhook dependencies

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-from async_lru import alru_cache
-from fastapi import FastAPI, HTTPException, Request
+import logging
+
 from google import genai
 from google.genai import types
-from pydantic import BaseModel
-from telegram import Bot, Update
+from telegram import Update
 from telegram.constants import ChatAction
-from telegram.error import BadRequest, InvalidToken, TimedOut
-from telegram.ext import Application, CallbackContext, ExtBot, MessageHandler, filters
+from telegram.ext import Application, CallbackContext, MessageHandler, filters
 
-from config import BOT_TOKEN, GOOGLE_API_KEY, SYSTEM_INSTRUCTIONS, TG_SECRET_TOKEN
+from config import BOT_TOKEN, GOOGLE_API_KEY, SYSTEM_INSTRUCTIONS
+
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+logging.getLogger("httpx").setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
+
 
 client = genai.Client(api_key=GOOGLE_API_KEY)
-
-
-app = FastAPI(
-    title="El Yeyo Fit",
-)
-
-
-class Url(BaseModel):
-    url: str
 
 
 async def messages(update: Update, context: CallbackContext):
@@ -94,48 +91,16 @@ async def messages(update: Update, context: CallbackContext):
         await update.message.reply_text(response.text, parse_mode="MARKDOWN")
 
 
-@alru_cache
-async def initialize_bot():
-    # pylint: disable=protected-access
-    application = Application.builder().token(BOT_TOKEN).updater(None).build()
-    bot = Bot(BOT_TOKEN)
-    bot_user = await bot.get_me()
-    bot_user._bot._initialized = True
-    extbot = ExtBot(token=bot.token)
-    extbot._bot_user = bot_user
-    extbot._initialized = True
-    application._initialized = True
-    application.bot = extbot
-    return application
+def main() -> None:
+    """Start the bot."""
+    application = Application.builder().token(BOT_TOKEN).build()
 
-
-async def process_telegram_event(update_json):
-    application = await initialize_bot()
-    update = Update.de_json(update_json, application.bot)
     application.add_handler(
         MessageHandler(filters=filters.ChatType.GROUPS, callback=messages)
     )
-    await application.process_update(update)
+
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 
-@app.post("/", tags=["Process Telegram Updates"])
-async def webhook(request: Request):
-    if request.headers.get("X-Telegram-Bot-Api-Secret-Token") == TG_SECRET_TOKEN:
-        update_json = await request.json()
-        try:
-            await process_telegram_event(update_json)
-        except TimedOut:
-            pass
-    return {"message": "ok"}
-
-
-@app.post("/setWebhook", tags=["Set bot webhook"])
-async def set_webhook(request: Request, data: Url):
-    """Use this method to specify a url and receive incoming updates via an outgoing webhook."""
-    if request.headers.get("X-Telegram-Bot-Api-Secret-Token") == TG_SECRET_TOKEN:
-        try:
-            await Bot(BOT_TOKEN).set_webhook(url=data.url, secret_token=TG_SECRET_TOKEN)
-            return {"message": "ok"}
-        except (InvalidToken, BadRequest, TimedOut) as exc:
-            raise HTTPException(status_code=400, detail=exc.message) from exc
-    raise HTTPException(status_code=400, detail="bad requests")
+if __name__ == "__main__":
+    main()
